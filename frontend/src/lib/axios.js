8ca1708a465173b6c454542224b27b730fe0291b
Siddharth Pandit <siddharth20pandit@gmail.com>
2025-07-03 11:51:05 -0500

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,9 +1,13 @@
+// lib/axios.js
+
 import axios from "axios";
 
-export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development"
+const baseURL =
+  import.meta.env.MODE === "development"
     ? "https://sidlink-backend.onrender.com/api"
-    : "/api", // 🔁 important
+    : "/api"; // <== make sure it's "/api" in production too
+
+export const axiosInstance = axios.create({
+  baseURL,
   withCredentials: true,
 });
-
