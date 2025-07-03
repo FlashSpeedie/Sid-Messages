a727c4128958faebff471f54bf03ea77aab23cd3
Siddharth Pandit <siddharth20pandit@gmail.com>
2025-07-02 23:57:24 -0500

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://sidlink-backend.onrender.com",
+    origin: "https://sidlink.onrender.com",
     credentials: true,
   })
 );
